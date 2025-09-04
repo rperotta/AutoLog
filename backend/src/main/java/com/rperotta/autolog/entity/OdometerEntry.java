45de41d022e885b406197f8ca36420f08e7f6723
rperotta <raulperotta7@gmail.com>
2025-09-04 17:22:47 +0200

    added OdometerEntry and ServiceRecord entities

--- backend/src/main/java/com/rperotta/autolog/entity/OdometerEntry.java
+++ backend/src/main/java/com/rperotta/autolog/entity/OdometerEntry.java
@@ -5,10 +5,29 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Entity
-@Table(name = "")
+@Table(name = "odometer_entries")
 @Getter
 @Setter
 @NoArgsConstructor
 public class OdometerEntry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int mileage;
+
+    private LocalDate date;
+
+    @Enumerated(EnumType.STRING)
+    private TireType tireType;
+
+    @ManyToOne
+    @JoinColumn(name = "vehicle_id", nullable = false)
+    private Vehicle vehicle;
+
+    private String notes;
 }
--- backend/src/main/java/com/rperotta/autolog/entity/ServiceRecord.java
+++ backend/src/main/java/com/rperotta/autolog/entity/ServiceRecord.java
@@ -5,10 +5,30 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Entity
-@Table(name = "")
+@Table(name = "service_records")
 @Getter
 @Setter
 @NoArgsConstructor
 public class ServiceRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate date;
+
+    private int mileage;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private ServiceType type;
+
+    @ManyToOne
+    @JoinColumn(name = "vehicle_id", nullable = false)
+    private Vehicle vehicle;
 }
+
